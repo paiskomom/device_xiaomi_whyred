agustkid/device_xiaomi_surya

surya: import parts from a11 tree
Signed-off-by: agustkid <agustkid@gmail.com>

interfaces/update-makefiles.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.xiaomi:device/xiaomi/ginkgo/interfaces"

paiskomom/device_xiaomi_whyred

Signed-off-by: rayrzy <vincentrayananda@gmail.com>
Change-Id: I1fdba1456431b2772a6845f05bb853d526defab9

interfaces/update-makefiles.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.xiaomi:device/xiaomi/surya/interfaces"