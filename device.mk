ad27f772a614eef4cd502bad42cc26f14050bd3f
haikaru17 <haikalluthfianinobalukia@proton.me>
2025-10-20 12:30:38 +0000

    whyred: drop `external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml`
    Signed-off-by: haikaru17 <haikalluthfianinobalukia@proton.me>

--- device.mk
+++ device.mk
@@ -142,9 +142,6 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     com.dsi.ant@1.0.vendor
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Biometrics
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.whyred
