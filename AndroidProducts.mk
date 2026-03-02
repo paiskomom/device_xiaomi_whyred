b848997e83beb7b1f5474ac80dcf5085dbd43ad0
paiskomom <midooneteam@gmail.com>
2026-03-03 06:42:12 +0700

    whyred: Qassa bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,14 +9,14 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WAqassaANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rr_whyred.mk
+    $(LOCAL_DIR)/qassa_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    rr_whyred-user \
-    rr_whyred-userdebug \
-    rr_whyred-eng
+    qassa_whyred-user \
+    qassa_whyred-userdebug \
+    qassa_whyred-eng
--- qassa_whyred.mk
+++ qassa_whyred.mk
@@ -20,7 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+$(call inherit-product, vendor/qassa/config/common_full_phone.mk)
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -31,7 +33,7 @@ $(call inherit-product, vendor/MiuiCamera/config.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := rr_whyred
+PRODUCT_NAME := qassa_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
